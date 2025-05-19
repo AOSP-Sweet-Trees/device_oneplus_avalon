e86f5b213f8538672cd9821c426667b7a8a188d7
koko-07870 <ko282ko82@gmail.com>
2025-05-19 20:15:22 +0530

    avalon: Initial device tree conversion
    Change-Id: I54af24af8721b5b60ae0862fcd0d29da0e7b7dd9
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- lineage_avalon.mk
+++ lineage_avalon.mk
@@ -9,23 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from waffle device
-$(call inherit-product, device/oneplus/waffle/device.mk)
+$(call inherit-product, device/oneplus/avalon/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_waffle
-PRODUCT_DEVICE := waffle
+PRODUCT_NAME := lineage_avalon
+PRODUCT_DEVICE := avalon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2581
+PRODUCT_MODEL := CPH2661
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="CPH2581EEA-user 15 AP3A.240617.008 U.R4T3.1d8d7d8-3fe7-3973 release-keys" \
-    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.1d8d7d8-3fe7-3973:user/release-keys \
-    DeviceName=OP595DL1 \
-    DeviceProduct=CPH2581 \
-    SystemDevice=OP595DL1 \
-    SystemName=CPH2581
+    BuildDesc="CPH2581EEA-user 15 AP3A.240617.008 U.R4T3.1c799fe_ca44_2 release-keys" \
+    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.1c799fe_ca44_2:user/release-keys \
+    DeviceName=OP5E93L1 \
+    DeviceProduct=CPH2661 \
+    SystemDevice=OP5E93L1 \
+    SystemName=CPH2661
